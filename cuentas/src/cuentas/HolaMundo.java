f75f6d46c7785d084791b5d4e538e70f6ca0a158
gustavoprieto <noticiasgus@gmail.com>
2021-02-02 17:38:11 +0100

    Primer commit Eclipse

---
+++ cuentas/src/cuentas/HolaMundo.java
@@ -0,0 +1,13 @@
+package cuentas;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola mundo");
+		
+
+	}
+
+}
